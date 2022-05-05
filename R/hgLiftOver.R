c50333d6381e0a9818db33d3838ab75b244c7fdb
mdelcorvo <marcello.delcorvo@gmail.com>
2022-05-05 15:06:35 +0200

    Update hgLiftOver.R

--- R/hgLiftOver.R
+++ R/hgLiftOver.R
@@ -1,1 +1,48 @@
+#' hgLiftOver
+#'
+#' Function to converts genome coordinates between assemblies. 
+#' 
+#' \code{hgLiftOver(data,from="hg38",to="hg19")}.
+#'
+#' @param df data.frame in BED format with chromosome number, start and end position or chromosome number and a single genomic position respectively
+#' @param from starting assembly
+#' @param to final assembly
+#' @return a messagge explaining if all packages are installed.
+#'
 
+#' @examples
+#' hgLiftOver(df = bed_file,from="hg38",to="hg19")
+#' @name hgLiftOver
+
+#' @export
+#' @rdname hgLiftOver
+
+#' @export
+#' @rdname hgLiftOver
+
+hgLiftOver <- function(df,from="hg38",to="hg19") {
+    
+    chain <- import.chain(paste(from,'To',to,'.over.chain',sep=''))
+    colnames(df)[1:3]<-c('Chrom','Start','End')
+    df_old<-df
+    
+    if (!all(grepl('chr',df$Chr))) {
+		df$Chrom <- paste('chr',df$Chrom,sep='')
+	}
+		
+    g1<-GRanges(df$Chr, ranges =IRanges(df$Start,df$End))
+	lift<-do.call(rbind.data.frame, lapply(lapply(liftOver(g1, chain),as.character),function(x) ifelse(length(x)==0,NA,x)))
+	colnames(lift)[1]<-to
+	
+	list1<-strsplit(lift[,1],':',fixed = TRUE)
+	list2<-strsplit(unlist(lapply(list1,function(x) x[2])),'-',fixed=T)
+	
+	df$Chrom<-unlist(lapply(list1,function(x) x[1]))
+	df$Start<- unlist(lapply(list2,function(x) x[1]))
+	df$End<-unlist(lapply(list2,function(x) x[2]))
+	df$refGenome <- to
+    if (!all(grepl('chr',df_old$Chr))) {
+		df$Chrom <- gsub('chr','',df$Chrom)
+	}
+    return(df)
+}
