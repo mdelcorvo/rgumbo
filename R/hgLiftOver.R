beb7dfc2f76454de5c01771db37c598f2b6153d1
mdelcorvo <marcello.delcorvo@gmail.com>
2022-05-11 04:08:10 +0200

    Update hgLiftOver.R

--- R/hgLiftOver.R
+++ R/hgLiftOver.R
@@ -2,16 +2,19 @@
 #'
 #' Function to converts genome coordinates between assemblies. 
 #' 
-#' \code{hgLiftOver(df,from="hg38",to="hg19")}.
+#' \code{hgLiftOver(bed,from="hg38",to="hg19",chrom=NULL,start=NULL,end=NULL)}.
 #'
-#' @param df data.frame in BED format with chromosome number, start and end position or chromosome number and a single genomic position respectively
+#' @param bed data.frame in BED format with chromosome number, start and end position or chromosome number and a single genomic position respectively
 #' @param from starting assembly
 #' @param to final assembly
+#' @param chrom Chromosome number
+#' @param start Start position
+#' @param end End position
 #' @return a messagge explaining if all packages are installed.
 #'
 
 #' @examples
-#' hgLiftOver(df = bed_file,from="hg38",to="hg19")
+#' hgLiftOver(bed,from="hg38",to="hg19")
 #' @name hgLiftOver
 
 #' @export
@@ -20,53 +23,104 @@
 #' @export
 #' @rdname hgLiftOver
 
-hgLiftOver <- function(df,from="hg38",to="hg19") {
+hgLiftOver <- function(bed,from="hg38",to="hg19",chrom=NULL,start=NULL,end=NULL) {
     
-    chain <- get(paste(from,'To',to,sep=''))
-    if (ncol(df)>2) {	
-	    colnames(df)[1:3]<-c('Chrom','Start','End')
-	    df_old<-df
-
-	    if (!all(grepl('chr',df$Chr))) {
-		df$Chrom <- paste('chr',df$Chrom,sep='')
-	    }
-
-	    g1<-GRanges(df$Chr, ranges =IRanges(df$Start,df$End))
-	    lift<-do.call(rbind.data.frame, lapply(lapply(liftOver(g1, chain),as.character),function(x) ifelse(length(x)==0,NA,x)))
-	    colnames(lift)[1]<-to
-						   
-	    list1<-strsplit(lift[,1],':',fixed = TRUE)
-	    list2<-strsplit(unlist(lapply(list1,function(x) x[2])),'-',fixed=T)
-					  
-	    df$Chrom<-unlist(lapply(list1,function(x) x[1]))
-	    df$Start<- unlist(lapply(list2,function(x) x[1]))
-	    df$End<-unlist(lapply(list2,function(x) x[2]))
-	    df$refGenome <- to
-				  
-	    if (!all(grepl('chr',df_old$Chr))) {
-		df$Chrom <- gsub('chr','',df$Chrom)
-	    }
-    } else {
-	    colnames(df)[1:2]<-c('Chrom','Pos')
-	    df_old<-df
+    chain <- get(paste(from,'To',to,sep='')) # load LiftOver annotation
+    if (ncol(bed)>2) {	
+			if (is.null(chrom)) {
+			colnames(bed)[1:3]<-c('Chrom','Start','End')
+			bed_old<-bed
+				
+				if (!all(grepl('chr',bed$Chr))) {
+				bed$Chrom <- paste('chr',bed$Chrom,sep='')
+				}
+				
+			g1<-GRanges(bed$Chr, ranges =IRanges(bed$Start,bed$End))
+			lift<-do.call(rbind.data.frame, lapply(lapply(liftOver(g1, chain),as.character),function(x) ifelse(length(x)==0,NA,x)))
+			colnames(lift)[1]<-to
+							   
+			list1<-strsplit(lift[,1],':',fixed = TRUE)
+			list2<-strsplit(unlist(lapply(list1,function(x) x[2])),'-',fixed=T)
+						  
+			bed$Chrom<-unlist(lapply(list1,function(x) x[1]))
+			bed$Start<- unlist(lapply(list2,function(x) x[1]))
+			bed$End<-unlist(lapply(list2,function(x) x[2]))
+			bed$refGenome <- to	
+					       
+				if (!all(grepl('chr',bed_old$Chr))) {
+				bed$Chrom <- gsub('chr','',bed$Chrom)
+				}	
+					       
+			} else if (is.null(end)) {
+			bed<-as.data.frame(bed)	
+			bed_old<-bed
 	    
-	    if (!all(grepl('chr',df$Chr))) {
-		df$Chrom <- paste('chr',df$Chrom,sep='')
-	    }
+				if (!all(grepl('chr',bed[,chrom]))) {
+				bed[,chrom] <- paste('chr',bed[,chrom],sep='')
+				}
 	    
-	    g1<-GRanges(df$Chr, ranges =IRanges(df$Pos,df$Pos))
-	    lift<-do.call(rbind.data.frame, lapply(lapply(liftOver(g1, chain),as.character),function(x) ifelse(length(x)==0,NA,x)))
-	    colnames(lift)[1]<-to
-		
-	    list1<-strsplit(lift[,1],':',fixed = TRUE)
-						   
-	    df$Chrom<-unlist(lapply(list1,function(x) x[1]))
-	    df$Pos<- unlist(lapply(list1,function(x) x[2]))
-	    df$refGenome <- to
+			g1<-GRanges(bed[,chrom], ranges =IRanges(bed[,start],bed[,start]))
+			lift<-do.call(rbind.data.frame, lapply(lapply(liftOver(g1, chain),as.character),function(x) ifelse(length(x)==0,NA,x)))
+			colnames(lift)[1]<-to
+
+			list1<-strsplit(lift[,1],':',fixed = TRUE)
+
+			bed[,chrom]<-unlist(lapply(list1,function(x) x[1]))
+			bed[,start]<- unlist(lapply(list1,function(x) x[2]))
+			bed$refGenome <- to
 				   
-	    if (!all(grepl('chr',df_old$Chr))) {
-		df$Chrom <- gsub('chr','',df$Chrom)
-	    }
-    }	    
-    return(df)
+				if (!all(grepl('chr',bed_old[,chrom]))) {
+				bed[,chrom] <- gsub('chr','',bed[,chrom])
+				}
+			
+			} else {
+			bed<-as.data.frame(bed)
+			bed_old<-bed
+			
+				if (!all(grepl('chr',bed[,chrom]))) {
+				bed[,chrom] <- paste('chr',bed[,chrom],sep='')
+				}
+				
+			g1<-GRanges(bed[,chrom], ranges =IRanges(bed[,start],bed[,end]))
+			lift<-do.call(rbind.data.frame, lapply(lapply(liftOver(g1, chain),as.character),function(x) ifelse(length(x)==0,NA,x)))
+			colnames(lift)[1]<-to
+							   
+			list1<-strsplit(lift[,1],':',fixed = TRUE)
+			list2<-strsplit(unlist(lapply(list1,function(x) x[2])),'-',fixed=T)
+						  
+			bed[,chrom]<-unlist(lapply(list1,function(x) x[1]))
+			bed[,start]<- unlist(lapply(list2,function(x) x[1]))
+			bed[,end]<-unlist(lapply(list2,function(x) x[2]))
+			bed$refGenome <- to
+			
+				if (!all(grepl('chr',bed_old$[,chrom]))) {
+				bed$[,chrom] <- gsub('chr','',bed$[,chrom])
+				}
+			}
+    } else {
+
+		        colnames(bed)[1:2]<-c('Chrom','Pos')
+		        bed_old<-bed
+	    
+				if (!all(grepl('chr',bed$Chr))) {
+				bed$Chrom <- paste('chr',bed$Chrom,sep='')
+				}
+	    
+			g1<-GRanges(bed$Chr, ranges =IRanges(bed$Pos,bed$Pos))
+			lift<-do.call(rbind.data.frame, lapply(lapply(liftOver(g1, chain),as.character),function(x) ifelse(length(x)==0,NA,x)))
+			colnames(lift)[1]<-to
+
+			list1<-strsplit(lift[,1],':',fixed = TRUE)
+
+			bed$Chrom<-unlist(lapply(list1,function(x) x[1]))
+			bed$Pos<- unlist(lapply(list1,function(x) x[2]))
+			bed$refGenome <- to
+				   
+				if (!all(grepl('chr',bed_old$Chr))) {
+				bed$Chrom <- gsub('chr','',bed$Chrom)
+				}
+		
+	}		    
+    return(bed)
 }
+
